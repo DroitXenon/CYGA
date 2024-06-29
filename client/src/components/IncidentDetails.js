ebac6f0957728215f9128c1a7f25e9298d115049
DroitXenon <droitinjuste@gmail.com>
2024-06-29 15:13:42 -0400

    Update layout

--- client/src/components/IncidentDetails.js
+++ client/src/components/IncidentDetails.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
-import { SpeedDial, SpeedDialIcon, SpeedDialAction,  Button, Box, Typography, Modal, Paper } from '@mui/material';
+import { Fab, SpeedDial, SpeedDialIcon, SpeedDialAction, Button, Box, Typography, Modal, Paper, Icon } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import AnalyticsIcon from '@mui/icons-material/Analytics';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Unstable_Grid2';
+import PersonIcon from '@mui/icons-material/Person';
+import SkateboardingIcon from '@mui/icons-material/Skateboarding';
+import PublicIcon from '@mui/icons-material/Public';
+import BackHandIcon from '@mui/icons-material/BackHand';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
 const darkTheme = createTheme({
   palette: {
@@ -29,64 +36,77 @@ function IncidentDetails({ incident }) {
     setAnalysisReport(null);
   };
 
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(3),
+    textAlign: 'left',
+    color: theme.palette.text.primary,
+  }));
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <Box sx={{ textAlign: 'center'}}>
-        {/* <Typography variant="h5" sx={{ mt: 2 }}>Details</Typography> */}
-        <Box className="incident-details" sx={{ textAlign: 'left', mt: 2}}>
-          <Box>
-            <Typography variant="h6">Attacker</Typography>
-            <Typography>Source IP: {incident.SourceIP}</Typography>
-            <Typography>Source Port: {incident.SourcePort}</Typography>
-            <Typography>Source Latitude: {incident.SourceLatitude}</Typography>
-            <Typography>Source Longitude: {incident.SourceLongitude} </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6">Victim</Typography>
-            <Typography>Target IP: {incident.DestinationIP}</Typography>
-            <Typography>Target Port: {incident.DestinationPort}</Typography>
-            <Typography>User Information: {incident.UserInfo}</Typography>
-            <Typography>Geolocation: {incident.GeoLocation}</Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6">Network Traffic</Typography>
-            <Typography>Protocol: {incident.Protocol}</Typography>
-            <Typography>Packet Length: {incident.PacketLength}</Typography>
-            <Typography>Packet Type: {incident.PacketType}</Typography>
-            <Typography>Traffic Type: {incident.TrafficType}</Typography>
-            <Typography>Segment: {incident.Segment}</Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6">Response</Typography>
-            <Typography>Anomaly Scores: {incident.AnomalyScores}</Typography>
-            <Typography>Action Taken: {incident.ActionTaken}</Typography>
-            <Typography>Severity Level: {incident.SeverityLevel}</Typography>
-            <Typography>Log Source: {incident.LogSource}</Typography>
-          </Box>
+      <Box sx={{ width: '100%', height: '60vh' }}>
+        <Typography variant="h4" align='center' sx={{ mt: 4, mb: 4 }}>Details</Typography>
+        <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 2 }} sx={{ height: 'calc(100% - 64px)' }}>
+          <Grid item xs={6} sx={{ display: 'flex' }}>
+            <Item sx={{ flex: 1 }}>
+              <Icon component={SkateboardingIcon} /><br />
+              <Typography variant="h6">Attacker</Typography>
+              Source IP: {incident.SourceIP}<br />
+              Source Port: {incident.SourcePort}<br />
+              Source Latitude: {incident.DestinationLatitude}<br />
+              Source Longitude: {incident.DestinationLongitude}
+            </Item>
+          </Grid>
+          <Grid item xs={6} sx={{ display: 'flex' }}>
+            <Item sx={{ flex: 1 }}>
+              <Icon component={PersonIcon} /><br />
+              <Typography variant="h6">Victim</Typography>
+              Target IP: {incident.DestinationIP}<br />
+              Target Port: {incident.DestinationPort}<br />
+              User Information: {incident.UserInfo}<br />
+              Geolocation: {incident.GeoLocation}<br />
+            </Item>
+          </Grid>
+          <Grid item xs={6} sx={{ display: 'flex' }}>
+            <Item sx={{ flex: 1 }}>
+              <Icon component={PublicIcon} /><br />
+              <Typography variant="h6">Network Traffic</Typography>
+              Protocol: {incident.Protocol}<br />
+              Packet Length: {incident.PacketLength}<br />
+              Packet Type: {incident.PacketType}<br />
+              Traffic Type: {incident.TrafficType}<br />
+              Segment: {incident.Segment}<br />
+            </Item>
+          </Grid>
+          <Grid item xs={6} sx={{ display: 'flex' }}>
+            <Item sx={{ flex: 1 }}>
+              <Icon component={BackHandIcon} /><br />
+              <Typography variant="h6">Response</Typography>
+              Anomaly Scores: {incident.AnomalyScores}<br />
+              Action Taken: {incident.ActionTaken}<br />
+              Severity Level: {incident.SeverityLevel}<br />
+              Log Source: {incident.LogSource}<br />
+            </Item>
+          </Grid>
+        </Grid>
+        <Box sx={{ position: 'absolute', bottom: 60, left: '50%', transform: 'translateX(-50%)', flexGrow: 1 }}>
+          <Fab color="primary" aria-label="add" size='medium'>
+            <VisibilityIcon />
+          </Fab>
+          <Fab variant="extended" onClick={handleAnalyze} sx={{ ml: 2 }}>
+            <AnalyticsIcon sx={{ mr: 1 }} />
+            Analysis
+          </Fab>
         </Box>
-        <Box sx={{ flexGrow: 1 }}>
-          <SpeedDial
-            ariaLabel="SpeedDial"
-            sx={{ position: 'absolute', bottom: 10, right: 10 }}
-            icon={<SpeedDialIcon />}
-            FabProps={{ color: 'outline' }}
-          >
-            <SpeedDialAction
-              icon={<AnalyticsIcon />}
-              tooltipTitle="Analysis"
-              onClick={handleAnalyze}
-            />
-          </SpeedDial>
-        </Box>
-        
-
-        <Modal open={isAnalysisModalOpen} onClose={handleCloseAnalysisModal}>
+        <Modal open={isAnalysisModalOpen} onClose={handleCloseAnalysisModal} align='center'>
           <Box
             sx={{
               p: 3,
               mx: 'auto',
-              mt: 5,
+              mt: '20vh',
               maxWidth: 500,
               maxHeight: '80vh',
               overflow: 'auto',
@@ -95,17 +115,17 @@ function IncidentDetails({ incident }) {
               borderRadius: 2,
             }}
           >
-            <Typography variant="h6" gutterBottom>
+            <Typography variant="h5">
               Report
             </Typography>
             {analysisReport ? (
               <Paper sx={{ p: 2 }}>
-                <Typography variant="body1">{analysisReport.report}</Typography>
+                <Typography variant="body1" align='left'>{analysisReport.report}</Typography>
               </Paper>
             ) : (
-              <Typography variant="body1">Loading analysis report...</Typography>
+              <Typography variant="body1" align='left'>Loading analysis report...</Typography>
             )}
-            <Button variant="contained" color="primary" onClick={handleCloseAnalysisModal} sx={{ mt: 2 }}>
+            <Button variant="outlined" color="primary" onClick={handleCloseAnalysisModal} sx={{ mt: 2 }}>
               Close
             </Button>
           </Box>
